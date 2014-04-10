f667c11f673815721106f521fcb52b09c22f53bd
fairk19 <fairk19@gmail.com>
2014-04-10 18:21:51 +0300

    all 85%

---
+++ src/test/java/messageSystem/MsgToWebSocketTest.java
@@ -0,0 +1,25 @@
+package messageSystem;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Alexandr on 10.04.14.
+ */
+public class MsgToWebSocketTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testExec() throws Exception {
+
+    }
+}
